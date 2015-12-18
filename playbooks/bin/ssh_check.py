e6de7e707491c230336ffa092059e786cabfa78b
Etsuji Nakai <enakai@redhat.com>
2015-12-18 15:25:47 +0900

    Fix: handle return code.

--- playbooks/bin/ssh_check.py
+++ playbooks/bin/ssh_check.py
@@ -3,12 +3,13 @@
 from subprocess import *
 import yaml, time, sys
 
-output = Popen( [ './openstack.py', '--list' ], stdout=PIPE )
+output = Popen(['./openstack.py', '--list'], stdout=PIPE)
 data = yaml.load(output.stdout)
 hostvars = data['_meta']['hostvars']
 
 args = sys.argv
 
+succeed = 0
 for host in hostvars:
     if not ('openstack' in hostvars[host]
             and 'ansible_ssh_host' in hostvars[host]):
@@ -18,14 +19,20 @@ for host in hostvars:
         continue
 
     addr = hostvars[host]['ansible_ssh_host']
-    count = 10
+    count = 12
     while count > 0:
-        try:
-            print "Checking if %s:%s is reachable.... " % (name, addr),
-            output = Popen( [ 'ssh', addr , 'uname' ], stdout=PIPE )
+        print "Checking if %s:%s is reachable.... " % (name, addr),
+        rc = call(['ssh', addr , 'uname'])
+        if rc == 0:
             print "ok."
-            count = 0
-        except:
+            succeed = 1
+            break
+        else:
             print "failed, try again."
             count -= 1
-            time.sleep(5)
+            time.sleep(10)
+
+if succeed != 1:
+    print "Failed."
+    sys.exit(1)
+
